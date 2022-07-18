c2176c3c3bae4dfc3a823a3f361c8103b89c1e6e
naresh <nareshsut@gmail.com>
2022-07-18 02:53:03 +0000

    Temporary Changes

--- final_project/machinetranslation/test.py
+++ final_project/machinetranslation/test.py
@@ -3,11 +3,11 @@ from translator import french_to_english,english_to_french
 
 class Test_translator(unittest.TestCase):
     def test_french_to_english(self):
-        self.assertEqual(french_to_english(""),"")
+        self.assertEqual(french_to_english(" ")," ")
         self.assertEqual(french_to_english("Bonjour"),"Hello")
 
     def test_english_to_french(self):
-        self.assertEqual(english_to_french(""),"")
+        self.assertEqual(english_to_french(" ")," ")
         self.assertEqual(english_to_french("Hello"),"Bonjour")
 
 
